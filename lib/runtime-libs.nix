f8e88c8081f1b6362e4e78f11222f3a1acbb8dae
Bad3r <25513724+Bad3r@users.noreply.github.com>
2026-02-09 18:28:35 +0300

    refactor(lib): replace deprecated xorg.* package names with top-level equivalents

--- lib/runtime-libs.nix
+++ lib/runtime-libs.nix
@@ -1,87 +1,82 @@
-pkgs:
-with pkgs;
-(
-  [
-    # Core toolchain bits expected by upstream bundles
-    glibc
-    curl
-    icu
-    libunwind
-    libuuid
-    lttng-ust
-    openssl
-    zlib
+pkgs: with pkgs; ([
+  # Core toolchain bits expected by upstream bundles
+  glibc
+  curl
+  icu
+  libunwind
+  libuuid
+  lttng-ust
+  openssl
+  zlib
 
-    # Mono/.NET friendly libraries
-    krb5
+  # Mono/.NET friendly libraries
+  krb5
 
-    # GTK / desktop integration
-    glib
-    gdk-pixbuf
-    gtk3
-    gtk4
-    libappindicator-gtk3
-    libnotify
-    libsecret
-    libxkbcommon
-    xdg-desktop-portal
-    xdg-user-dirs
-    xdg-utils
-    pipewire
-    systemd
-    udev
-    libudev0-shim
+  # GTK / desktop integration
+  glib
+  gdk-pixbuf
+  gtk3
+  gtk4
+  libappindicator-gtk3
+  libnotify
+  libsecret
+  libxkbcommon
+  xdg-desktop-portal
+  xdg-user-dirs
+  xdg-utils
+  pipewire
+  systemd
+  udev
+  libudev0-shim
 
-    # Audio
-    alsa-lib
-    libpulseaudio
-    speechd-minimal
+  # Audio
+  alsa-lib
+  libpulseaudio
+  speechd-minimal
 
-    # Font stack
-    dejavu_fonts
-    fontconfig
-    freetype
-    harfbuzz
-    pango
-    cairo
+  # Font stack
+  dejavu_fonts
+  fontconfig
+  freetype
+  harfbuzz
+  pango
+  cairo
 
-    # Media / GPU
-    libdrm
-    libGL
-    libglvnd
-    libgbm
-    mesa
-    libva
-    libvdpau
-    vulkan-loader
-    pciutils
-    stdenv.cc.cc
-    cups
+  # Media / GPU
+  libdrm
+  libGL
+  libglvnd
+  libgbm
+  mesa
+  libva
+  libvdpau
+  vulkan-loader
+  pciutils
+  stdenv.cc.cc
+  cups
 
-    # Runtime libraries used by Electron extensions
-    nspr
-    nss
-    dbus
-    at-spi2-atk
-    at-spi2-core
-    expat
-  ]
-  ++ (with pkgs.xorg; [
-    libX11
-    libXScrnSaver
-    libXcomposite
-    libXcursor
-    libXdamage
-    libXext
-    libXfixes
-    libxkbfile
-    libXrandr
-    libXrender
-    libXtst
-    libXi
-    libxcb
-    libxshmfence
-    libXau
-    libXdmcp
-  ])
-)
+  # Runtime libraries used by Electron extensions
+  nspr
+  nss
+  dbus
+  at-spi2-atk
+  at-spi2-core
+  expat
+  # X11 / Xorg
+  libx11
+  libxscrnsaver
+  libxcomposite
+  libxcursor
+  libxdamage
+  libxext
+  libxfixes
+  libxkbfile
+  libxrandr
+  libxrender
+  libxtst
+  libxi
+  libxcb
+  libxshmfence
+  libxau
+  libxdmcp
+])
